6884dcd8e8a48e32153ea210957e9b3b9a76ed93
Falcon Tech <78836383+nav-github01001@users.noreply.github.com>
2022-10-25 17:05:48 +0800

    Add essential files

---
+++ MDinterpret.py
@@ -0,0 +1,13 @@
+import os
+import platform
+import exceptions
+# Command line start
+
+print("Welcome to MDparse!")
+print("A MarkDown->HTML conversion utility")
+
+
+file = input("File to convert to HTML?")
+
+
+
